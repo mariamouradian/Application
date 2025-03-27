2ba063f1b2a727e30c427ab46a31a9c87815a600
Maria_Mouradian <mariamouradian@yandex.ru>
2025-03-28 01:04:34 +0300

    HW Added

---
+++ Seminar5/Seminar5/Seminar5/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Seminar5
+{
+    internal class Program
+    {
+        static void Calculator_GotResult(object sendler, EventArgs eventArgs)
+        {
+            Console.WriteLine($"{((Calculator)sendler).Result}");
+        }
+
+        static void Main(string[] args)
+        {
+            ICalc calc = new Calculator();
+
+            calc.GotResult += Calculator_GotResult;
+            
+            calc.Sum(2);
+            calc.Substruct(4);
+            calc.Multiply(3);
+            calc.CancelLast();
+            calc.Multiply(5);
+
+        }
+    }
+}
